4b8ce2cdab4586403b4dfb3fb5973f0818f172b7
Nat Welch <nat@natwelch.com>
2017-03-09 08:59:01 -0800

    refactor

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"golang.org/x/net/html"
@@ -12,9 +13,20 @@ func main() {
 	if len(os.Args) != 2 {
 		log.Fatalf("Usage: %s URL", os.Args[0])
 	}
-	response, err := http.Get(os.Args[1])
+
+	urls, err := ScrapeUrl(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
+	}
+	log.Printf("%v", urls)
+}
+
+func ScrapeUrl(uri string) ([]string, error) {
+	response, err := http.Get(uri)
+	ret := []string{}
+
+	if err != nil {
+		return nil, err
 	} else {
 		defer response.Body.Close()
 		z := html.NewTokenizer(response.Body)
@@ -25,18 +37,28 @@ func main() {
 			switch {
 			case tt == html.ErrorToken:
 				// End of the document, we're done
-				return
+				return ret, nil
 			case tt == html.StartTagToken:
 				t := z.Token()
 
 				if t.Data == "a" {
 					for _, attr := range t.Attr {
 						if attr.Key == "href" {
-							log.Printf("%+v", attr.Val)
+							u, err := url.ParseRequestURI(attr.Val)
+							if err != nil {
+								continue
+							} else {
+								if u.IsAbs() {
+									log.Printf("Found %+v", attr.Val)
+									ret = append(ret, attr.Val)
+								}
+							}
 						}
 					}
 				}
 			}
 		}
 	}
+
+	return ret, nil
 }
